2fcb85041b29395c728f1e4974391ac68385f0a8
Sean Miller <smiller206@me.com>
2019-03-11 16:23:18 -0700

    completed Auth0 and deployment

--- InstaHub-MVC/InstaHub-MVC/Startup.cs
+++ InstaHub-MVC/InstaHub-MVC/Startup.cs
@@ -95,12 +95,13 @@ namespace InstaHub_MVC
             });
 
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
