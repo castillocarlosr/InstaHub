a8592914ac52a8bfd65f0370d54868f625bd63a9
Sean Miller <smiller206@me.com>
2019-03-11 14:41:31 -0700

    added Auth0 with Google

---
+++ InstaHub-FrontEnd/InstaHub-FrontEnd/ClientApp/src/components/chat.tsx
@@ -0,0 +1,44 @@
+﻿import * as React from 'react';
+import * as moment from 'moment';
+
+interface ChatState {
+    messages: ChatMessage[];
+    currentMessage: string;
+}
+interface ChatMessage {
+    id: number;
+    date: Date;
+    message: string;
+    sender: string;
+}
+export class Chat extends React.Component<{}, ChatState> {
+    public render() {
+        return <div className='panel panel-default'>
+            <div className='panel-body panel-chat'
+                ref={this.handlePanelRef}>
+                <ul>
+                    {this.state.messages.map(message =>
+                        <li key={message.id}><strong>{message.sender} </strong>
+                            ({moment(message.date).format('HH:mm:ss')})<br />
+                            {message.message}</li>
+                    )}
+                </ul>
+            </div>
+            <div className='panel-footer'>
+                <form className='form-inline' onSubmit={this.onSubmit}>
+                    <label className='sr-only' htmlFor='msg'>Message</label>
+                    <div className='input-group col-md-12'>
+                        <button className='chat-button input-group-addon'>:-)</button>
+                        <input type='text' value={this.state.currentMessage}
+                            onChange={this.handleMessageChange}
+                            className='form-control'
+                            id='msg'
+                            placeholder='Your message'
+                            ref={this.handleMessageRef} />
+                        <button className='chat-button input-group-addon'>Send</button>
+                    </div>
+                </form>
+            </div>
+        </div>;
+    }
+}
