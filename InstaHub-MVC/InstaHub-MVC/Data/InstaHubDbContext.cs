3437a358ed17e495d7e7bff3dae1b3955b02028c
andrewquamme <andrewquamme@gmail.com>
2019-03-11 14:35:58 -0700

    create InstaHub DB Context

--- InstaHub-MVC/InstaHub-MVC/Data/InstaHubDbContext.cs
+++ InstaHub-MVC/InstaHub-MVC/Data/InstaHubDbContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InstaHub_MVC.Data
 {
-    public class InstaHubDbContext
+    public class InstaHubDbContext : DbContext
     {
+        public InstaHubDbContext(DbContextOptions<InstaHubDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
     }
 }
