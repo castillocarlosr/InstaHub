58e0727c1b7a20c01a690013360125df400723ec
castillocarlosr <castillocarlosr@gmail.com>
2019-03-12 11:59:56 -0700

    SetuP almost done

--- InstaHub-MVC/InstaHub-MVC/Data/InstaHubDbContext.cs
+++ InstaHub-MVC/InstaHub-MVC/Data/InstaHubDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using InstaHub_MVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
--- InstaHub-MVC/InstaHub-MVC/Models/ApplicationUser.cs
+++ InstaHub-MVC/InstaHub-MVC/Models/ApplicationUser.cs
@@ -1,19 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace InstaHub_MVC.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
-        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
         public string Description { get; set; }
         public bool Online { get; set; }
+        public ClaimsIdentity Email { get; internal set; }
 
         public ICollection<Contact> Contacts;
         public ICollection<Message> Messages;
         public ICollection<Hub> Hubs;
 
+    }
+
+    public static class ApplicationRoles
+    {
+        public const string Member = "Member";
     }
 }
