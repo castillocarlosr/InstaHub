281b72d7a67a1b69f3abe8d8c05c0433fe6f0a7b
andrewquamme <andrewquamme@gmail.com>
2019-03-12 11:57:10 -0700

    add primary key to ApplicationUser model

--- InstaHub-MVC/InstaHub-MVC/Models/ApplicationUser.cs
+++ InstaHub-MVC/InstaHub-MVC/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,8 @@ namespace InstaHub_MVC.Models
 {
     public class ApplicationUser
     {
-        public int UserID { get; set; }
+        [Key]
+        public string UserID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public bool Online { get; set; }
