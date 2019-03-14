eebb52fb35c88d4768c96e21e436601b0926ace5
castillocarlosr <castillocarlosr@gmail.com>
2019-03-13 17:03:14 -0700

    Pushing application services

---
+++ InstaHub-MVC/InstaHub-MVC/Models/Interfaces/IAppUser.cs
@@ -0,0 +1,17 @@
+﻿using InstaHub_MVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaHub_MVC.Models.Interfaces
+{
+    public interface IAppUser
+    {
+        Task AddAppUser(ApplicationUser AppUser);
+
+        Task<ApplicationUser> GetApplicationUserByID(string id);
+
+        Task<Group> GetUserGroupAsync(int id);  
+    }
+}
---
+++ InstaHub-MVC/InstaHub-MVC/Models/Services/ApplicationUserServices.cs
@@ -0,0 +1,40 @@
+﻿using InstaHub_MVC.Data;
+using InstaHub_MVC.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaHub_MVC.Models.Services
+{
+    public class ApplicationUserServices : IAppUser
+    {
+        private InstaHubDbContext _context { get; }
+
+        public ApplicationUserServices(InstaHubDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAppUser(ApplicationUser AppUser)
+        {
+            _context.ApplicationUsers.Add(AppUser);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ApplicationUser> GetApplicationUserByID(string id)
+        {
+            ApplicationUser applicationUser = await _context.ApplicationUsers.FirstOrDefaultAsync(u => u.Id == id);
+            return applicationUser;
+        }
+
+        public async Task<Group> GetUserGroupAsync(int id)
+        {
+            //Group group = await _context.Hubs.Where(g => g.Id)
+            Group group = await _context.UserHubs.FirstOrDefaultAsync(g => g.Id == id);
+            group.GroupType = await _context.Hubs.Where(g => g.HubsID == group.ID).Include("Message").ToListAsync();
+            return group;
+        }
+    }
+}
