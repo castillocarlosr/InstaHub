75727324fa558c251013bb625870dff14052d842
Sean Miller <smiller206@me.com>
2019-03-14 20:14:18 -0700

    updated logout return url to go to our account login

--- InstaHub-MVC/InstaHub-MVC/Controllers/AccountController.cs
+++ InstaHub-MVC/InstaHub-MVC/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace InstaHub_MVC.Controllers
                 // Indicate here where Auth0 should redirect the user after a logout.
                 // Note that the resulting absolute Uri must be whitelisted in the 
                 // **Allowed Logout URLs** settings for the client.
-                RedirectUri = Url.Action("Index", "Home")
+                RedirectUri = Url.Action("Login", "Account")
             });
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
