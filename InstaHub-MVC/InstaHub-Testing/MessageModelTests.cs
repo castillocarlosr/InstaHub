8b94926db20243fcf38ac04a0ec532ec7aa16296
andrewquamme <andrewquamme@gmail.com>
2019-03-14 18:41:44 -0700

    Update Message Model tests to match changes made to data model

--- InstaHub-MVC/InstaHub-Testing/MessageModelTests.cs
+++ InstaHub-MVC/InstaHub-Testing/MessageModelTests.cs
@@ -14,21 +14,21 @@ namespace InstaHub_Testing
             Assert.Equal(1, message.ID);
         }
 
-        //[Fact]
-        //public void UserIDGetSet()
-        //{
-        //    Message message = new Message();
-        //    message.UserName = 1;
-        //    Assert.Equal(1, message.UserName);
-        //}
+        [Fact]
+        public void UserNameGetSet()
+        {
+            Message message = new Message();
+            message.UserName = "KeithMagnuson3@email.com";
+            Assert.Equal("KeithMagnuson3@email.com", message.UserName);
+        }
 
-        //[Fact]
-        //public void GroupIDGetSet()
-        //{
-        //    Message message = new Message();
-        //    message.GroupName = 1;
-        //    Assert.Equal(1, message.GroupName);
-        //}
+        [Fact]
+        public void GroupNameGetSet()
+        {
+            Message message = new Message();
+            message.GroupName = "Chicago Blackhaws Fans";
+            Assert.Equal("Chicago Blackhaws Fans", message.GroupName);
+        }
 
         [Fact]
         public void ValueGetSet()
